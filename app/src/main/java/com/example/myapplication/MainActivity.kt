7479e72f97d9c6690900d7c53224d3cc0d5b52cf
Manhatai <matixxx.mp4@gmail.com>
2023-12-13 18:43:46 +0100

    First commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() , View.OnClickListener {
+
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMultiply : Button
+    lateinit var btnDivision : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTv : TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_sub)
+        btnMultiply = findViewById(R.id.btn_multiplication)
+        btnDivision = findViewById(R.id.btn_division)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultTv = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMultiply.setOnClickListener(this)
+        btnDivision.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        var a = etA.text.toString().toDouble()
+        var b = etB.text.toString().toDouble()
+        var result = 0.0
+        when(v?.id){
+            R.id.btn_add ->{
+                result = a + b
+            }
+            R.id.btn_sub ->{
+                result = a - b
+            }
+            R.id.btn_multiplication ->{
+                result = a * b
+            }
+            R.id.btn_division ->{
+                result = a / b
+            }
+        }
+        resultTv.text = "Result is $result"
+    }
+}
